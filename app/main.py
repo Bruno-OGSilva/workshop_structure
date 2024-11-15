d3a036a6da180d18e82968ff83ba8b6b56fba2dd
Bruno Silva <bruno.ogs@gmail.com>
2024-11-14 21:14:16 -0500

    tests working

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import concatenate_dataframes
+from pipeline.load import load_excel
 
-dataframe_lists = extract_from_excel("data/input")
 
-print(dataframe_lists)
+if __name__ =="__main__":
+    dataframe_lists = extract_from_excel("data/input")
+    data_frame = concatenate_dataframes(dataframe_lists)
+    load_excel(data_frame, "data/output", "output")
---
+++ app/pipeline/load.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+
+
+def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+    
+    """
+    receive a dataframe and save it as MS Excel
+
+    args: 
+    data_frame (pd.DataFrame): dataframe to be saved as excel
+    output_path (str): parth where the file will be saved
+    file_name (str): file name to be saved
+
+    return: "File saved successfuly"
+    """
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    
+    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
+    return "File saved successfuly"
